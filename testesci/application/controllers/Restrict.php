d299c978c4c987f326db53abac2b2f92c483f913
Rodrigo Mello <hollowrm08@gmail.com>
2020-07-27 16:11:50 -0300

    commit 4

--- testesci/application/controllers/Restrict.php
+++ testesci/application/controllers/Restrict.php
@@ -3,7 +3,10 @@
 
     class Restrict Extends CI_Controller{
         public function index(){
-            $this->template->show("login.php");
+            //$this->template->show("login.php");
+
+            $this->load->model("users_model");
+            print_r($this->users_model->get_user_data("hollowrm08"));
         }
 
         
